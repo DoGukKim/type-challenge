87c960f8e5a3ffef3647c56c04c2f1c61b24bd49
DoGukKim <dgkim3811@naver.com>
2023-04-28 09:44:48 +0900

    타입 챌린지 풀이

--- medium/00459-medium-flatten-pending.ts
+++ medium/00459-medium-flatten-pending.ts
@@ -2,27 +2,30 @@
 import type { Equal, Expect } from "../test-utils";
 
 type cases = [
-  Expect<Equal<Flatten<[]>, []>>
+  Expect<Equal<Flatten<[]>, []>>,
   Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
   Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-  Expect<Equal<Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>, [{ foo: 'bar'; 2: 10 }, 'foobar']>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: "bar"; 2: 10 }, "foobar"]>,
+      [{ foo: "bar"; 2: 10 }, "foobar"]
+    >
+  >
 ];
 
 // ============= Your Code Here =============
-// type Flatten<T extends unknown[]> = 
+type Flatten<T extends unknown[], A extends unknown[] = []> = T extends [
+  infer F,
+  ...infer R
+]
+  ? F extends unknown[]
+    ? Flatten<[...F, ...R], A>
+    : Flatten<[...R], [...A, F]>
+  : A;
 
-// -----
-
-
-// type Flatten<S extends any[], T extends any[] = []> =  S extends [infer X, ...infer Y] ? 
-// X extends any[] ?
-//  Flatten<[...X, ...Y], T> : Flatten<[...Y], [...T, X]> 
-// : T
-
-
-  // type Flatten<T> = T extends []
-  //   ? []
-  //   : T extends [infer First, ...infer Rest]
-  //   ? [...Flatten<First>, ...Flatten<Rest>]
-  //   : [T];
+// type Flatten<T> = T extends []
+//   ? []
+//   : T extends [infer First, ...infer Rest]
+//   ? [...Flatten<First>, ...Flatten<Rest>]
+//   : [T];
