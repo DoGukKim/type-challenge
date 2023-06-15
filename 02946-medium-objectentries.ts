f81e82fac3acafc96a2c6d4ae350e1114237a923
DoGukKim <dgkim3811@naver.com>
2023-06-15 10:36:42 +0900

    타입 챌린지 풀이

---
+++ 02946-medium-objectentries.ts
@@ -0,0 +1,36 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries =
+  | ["name", string]
+  | ["age", number]
+  | ["locations", string[] | null];
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ["key", undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ["key", undefined]>>
+];
+
+// ============= Your Code Here =============
+
+type ObjectEntries<T> = {
+  [K in keyof T]-?: [
+    K,
+    T[K] extends undefined ? T[K] : Exclude<T[K], undefined>
+  ];
+}[keyof T];
+
+// my solution
+// type ObjectEntries<T, K = { [P in keyof T]-?: T[P] }> = keyof K extends infer U
+//   ? U extends U
+//     ? [U, K[U & keyof K] extends never ? undefined : K[U & keyof K]]
+//     : never
+//   : never;
