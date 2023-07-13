abb09c6235fdcbe91b161b20918d505ca88742c4
DoGukKim <dgkim3811@naver.com>
2023-07-13 13:37:46 +0900

    타입 챌린지 문제 풀이 및 복습

---
+++ 08767-medium-combination.ts
@@ -0,0 +1,32 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<
+    Equal<
+      Combination<["foo", "bar", "baz"]>,
+      | "foo"
+      | "bar"
+      | "baz"
+      | "foo bar"
+      | "foo bar baz"
+      | "foo baz"
+      | "foo baz bar"
+      | "bar foo"
+      | "bar foo baz"
+      | "bar baz"
+      | "bar baz foo"
+      | "baz foo"
+      | "baz foo bar"
+      | "baz bar"
+      | "baz bar foo"
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type Combination<
+  T extends string[],
+  U extends string = T[number],
+  All extends string = U
+> = U extends U ? U | `${U} ${Combination<T, Exclude<All, U>>}` : never;
