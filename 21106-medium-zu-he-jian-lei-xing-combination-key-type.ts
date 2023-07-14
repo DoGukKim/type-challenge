a7c81976f4d2220ccd2e76c9e91e7c7b61803ac6
DoGukKim <dgkim3811@naver.com>
2023-07-14 09:45:31 +0900

    타입 챌린지 문제 풀이 및 복습

---
+++ 21106-medium-zu-he-jian-lei-xing-combination-key-type.ts
@@ -0,0 +1,15 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type CaseTypeOne = 'cmd ctrl' | 'cmd opt' | 'cmd fn' | 'ctrl opt' | 'ctrl fn' | 'opt fn'
+
+type cases = [
+  Expect<Equal<Combs, CaseTypeOne>>,
+]
+
+
+// ============= Your Code Here =============
+type ModifierKeys = ['cmd', 'ctrl', 'opt', 'fn']
+
+// 实现 Combs
+type Combs = any
