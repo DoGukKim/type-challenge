197fb3fa4937d76d91ab12c8bb12b24c8cf338d3
DoGukKim <doguk1994@gmail.com>
2025-10-21 13:46:38 +0900

    문제풀이

---
+++ last-dance/00062-medium-type-lookup.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type Animal = Cat | Dog;
+
+type cases = [
+  Expect<Equal<LookUp<Animal, "dog">, Dog>>,
+  Expect<Equal<LookUp<Animal, "cat">, Cat>>
+];
+
+// ============= Your Code Here =============
+type LookUp<
+  T extends { type: Animal["type"] },
+  U extends T["type"]
+> = T extends { type: U } ? T : never;
