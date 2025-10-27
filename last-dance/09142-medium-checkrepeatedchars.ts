deaf7e5bf39c43cba088389334b0e986726c5cce
DoGukKim <doguk1994@gmail.com>
2025-10-27 10:32:44 +0900

    미디엄 문제풀이

---
+++ last-dance/09142-medium-checkrepeatedchars.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<CheckRepeatedChars<"abc">, false>>,
+  Expect<Equal<CheckRepeatedChars<"abb">, true>>,
+  Expect<Equal<CheckRepeatedChars<"cbc">, true>>,
+  Expect<Equal<CheckRepeatedChars<"">, false>>
+];
+
+// ============= Your Code Here =============
+type CheckRepeatedChars<T extends string> = T extends `${infer C}${infer R}`
+  ? R extends `${string}${C}${string}`
+    ? true
+    : CheckRepeatedChars<R>
+  : false;
