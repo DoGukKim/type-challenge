5ac76b59890713b5941087c5e14d7242568c5615
DoGukKim <doguk1994@gmail.com>
2025-11-03 10:13:01 +0900

    미디엄 문제풀이

---
+++ last-dance/27932-medium-mergeall.ts
@@ -0,0 +1,35 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<MergeAll<[]>, {}>>,
+  Expect<Equal<MergeAll<[{ a: 1 }]>, { a: 1 }>>,
+  Expect<Equal<MergeAll<[{ a: string }, { a: string }]>, { a: string }>>,
+  Expect<Equal<MergeAll<[{}, { a: string }]>, { a: string }>>,
+  Expect<Equal<MergeAll<[{ a: 1 }, { c: 2 }]>, { a: 1; c: 2 }>>,
+  Expect<
+    Equal<
+      MergeAll<[{ a: 1; b: 2 }, { a: 2 }, { c: 3 }]>,
+      { a: 1 | 2; b: 2; c: 3 }
+    >
+  >,
+  Expect<Equal<MergeAll<[{ a: 1 }, { a: number }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: number }, { a: 1 }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>
+];
+
+// ============= Your Code Here =============
+type FlatObj<T> = { [P in keyof T]: T[P] };
+type MergeAll<T, O extends Record<PropertyKey, any> = {}> = T extends [
+  infer F,
+  ...infer R
+]
+  ? MergeAll<
+      R,
+      FlatObj<
+        Omit<O, keyof F> & {
+          [P in keyof F]: P extends keyof O ? F[P] | O[P] : F[P];
+        }
+      >
+    >
+  : O;
