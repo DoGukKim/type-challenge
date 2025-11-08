b600b45f86529b164ab9cd20fc47de588f051352
DoGukKim <doguk1994@gmail.com>
2025-11-08 15:38:28 +0900

    미디엄 문제풀이 flattendepth

---
+++ last-dance/03243-medium-flattendepth.ts
@@ -0,0 +1,28 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<
+    Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>
+  >
+];
+
+// ============= Your Code Here =============
+type NestedArray = (number | NestedArray)[];
+type FlattenDepth<
+  T extends NestedArray,
+  S extends number = 1,
+  U extends NestedArray = []
+> = U["length"] extends S
+  ? T
+  : T extends [infer F, ...infer R extends NestedArray]
+  ? F extends NestedArray
+    ? [...FlattenDepth<F, S, [...U, 1]>, ...FlattenDepth<R, S, U>]
+    : [F, ...FlattenDepth<R, S, U>]
+  : T;
